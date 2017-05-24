de4e4a1634d96953f5be1465f69e145067018d54
tomayac <steiner.thomas@gmail.com>
2017-05-24 11:10:57 +0200

    Dynamically loading Firebase if needed.

--- client/js/instance-loader.js
+++ client/js/instance-loader.js
@@ -11,83 +11,102 @@
         throw Error(`Retriving ${id} failed with status code ${
             response.statusCode}`);
       })
-      .catch((fetchError) => fetchError);
+      .catch((fetchError) => Promise.reject(fetchError));
     },
   },
 
   PWASSEMBLE.firebaseLoader = {
     load(id) {
-      const config = {
-        apiKey: 'AIzaSyCmQzeCh9nFg5SCwbfsPDkpigwat_fuW68',
-        authDomain: 'pwassemble-1337.firebaseapp.com',
-        databaseURL: 'https://pwassemble-1337.firebaseio.com',
-        storageBucket: 'pwassemble-1337.appspot.com',
-      };
-      firebase.initializeApp(config);
-      const db = firebase.database();
-      const storage = firebase.storage();
+      // Dynamically load the Firebase SDK
+      return Promise.all(
+        [
+          'firebase-app.js',
+          'firebase-database.js',
+          'firebase-storage.js',
+        ]
+        .map((src) => {
+          return new Promise((resolve, reject) => {
+            const script = document.createElement('script');
+            script.addEventListener('load', (e) => resolve(src));
+            script.addEventListener('error', (e) => reject(src));
+            script.src = `https://www.gstatic.com/firebasejs/3.6.1/${src}`;
+            document.head.appendChild(script);
+          });
+        })
+      )
+      .then(() => {
+        const config = {
+          apiKey: 'AIzaSyCmQzeCh9nFg5SCwbfsPDkpigwat_fuW68',
+          authDomain: 'pwassemble-1337.firebaseapp.com',
+          databaseURL: 'https://pwassemble-1337.firebaseio.com',
+          storageBucket: 'pwassemble-1337.appspot.com',
+        };
+        firebase.initializeApp(config);
+        const db = firebase.database();
+        const storage = firebase.storage();
 
-      return db.ref(`/instances/${id}`)
-      .once('value')
-      .then((snapshot) => snapshot.val())
-      .then((results) => {
-        if (!results) {
-          return Promise.reject(`Could not find instance ${id}`);
-        }
-        const promises = [Promise.resolve(results)];
-        const dynamicComponents = [
-          'companyLogoImgId',
-          'heroImgId',
-          'iconImgId',
-        ];
-        dynamicComponents.forEach((dynamicComponent) => {
-          let dynamicKey = false;
-          if ((dynamicComponent === 'companyLogoImgId') &&
-              (results.companyLogoImgName)) {
-            dynamicKey =
-                `${results[dynamicComponent]}.${results.companyLogoImgName
-                .split('.')[1]}`;
-          } else if ((dynamicComponent === 'heroImgId') &&
-                     (results.heroImgId)) {
-            dynamicKey =
-                `${results[dynamicComponent]}.${results.heroImgName
-                .split('.')[1]}`;
-          } else if ((dynamicComponent === 'iconImgId') &&
-                     (results.iconImgId)) {
-            dynamicKey =
-                `${results[dynamicComponent]}.${results.iconImgName
-                .split('.')[1]}`;
+        return db.ref(`/instances/${id}`)
+        .once('value')
+        .then((snapshot) => snapshot.val())
+        .then((results) => {
+          if (!results) {
+            return Promise.reject(`Could not find instance ${id}`);
           }
-          if (!dynamicKey) {
-            return;
+          const promises = [Promise.resolve(results)];
+          const dynamicComponents = [
+            'companyLogoImgId',
+            'heroImgId',
+            'iconImgId',
+          ];
+          dynamicComponents.forEach((dynamicComponent) => {
+            let dynamicKey = false;
+            if ((dynamicComponent === 'companyLogoImgId') &&
+                (results.companyLogoImgName)) {
+              dynamicKey =
+                  `${results[dynamicComponent]}.${results.companyLogoImgName
+                  .split('.')[1]}`;
+            } else if ((dynamicComponent === 'heroImgId') &&
+                       (results.heroImgId)) {
+              dynamicKey =
+                  `${results[dynamicComponent]}.${results.heroImgName
+                  .split('.')[1]}`;
+            } else if ((dynamicComponent === 'iconImgId') &&
+                       (results.iconImgId)) {
+              dynamicKey =
+                  `${results[dynamicComponent]}.${results.iconImgName
+                  .split('.')[1]}`;
+            }
+            if (!dynamicKey) {
+              return;
+            }
+            /* eslint-disable max-nested-callbacks */
+            promises.push(
+              storage.ref(dynamicKey).getDownloadURL()
+              .then((url) => {
+                return {
+                  dynamicComponent,
+                  dynamicKey,
+                  url,
+                };
+              })
+            );
+            /* eslint-enable max-nested-callbacks */
+          });
+          return Promise.all(promises);
+        })
+        .then((results) => {
+          let finalResults = results[0];
+          results = results.slice(1);
+          results.forEach((result) => {
+            finalResults[result.dynamicComponent] = result.url;
+          });
+          if (PWASSEMBLE.DEBUG_MODE) {
+            console.log(PWASSEMBLE.DEBUG_PREFIX,
+                'Storing instance data to IndexedDB');
           }
-          /* eslint-disable max-nested-callbacks */
-          promises.push(
-            storage.ref(dynamicKey).getDownloadURL()
-            .then((url) => {
-              return {
-                dynamicComponent,
-                dynamicKey,
-                url,
-              };
-            })
-          );
-          /* eslint-enable max-nested-callbacks */
+          idbKeyval.set(id, finalResults);
+          return Promise.resolve(finalResults);
         });
-        return Promise.all(promises);
-      })
-      .then((results) => {
-        let finalResults = results[0];
-        results = results.slice(1);
-        results.forEach((result) => {
-          finalResults[result.dynamicComponent] = result.url;
-        });
-        if (PWASSEMBLE.DEBUG_MODE) {
-          console.log(PWASSEMBLE.DEBUG_PREFIX,
-              'Storing instance data to IndexedDB');
-        }
-        idbKeyval.set(id, finalResults);
-        return Promise.resolve(finalResults);
       });
     },
   },
--- client/service-worker.js
+++ client/service-worker.js
@@ -50,6 +50,11 @@ REQUEST_STRATEGIES.set(/www\.googleapis\.com/, {
   strategy: 'networkFirst',
   cache: true,
 });
+// Remote Firebase static resources
+REQUEST_STRATEGIES.set(/firebasestorage\.googleapis\.com/, {
+  strategy: 'cacheFirst',
+  cache: true,
+});
 
 const addToCache = (request, networkResponse) => {
   const requestUrl = request.url;
